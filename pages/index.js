171a6fd28c551eb9c35b69400dc341b85aacb540
Nikola Gavran <nikolagavran16@gmail.com>
2025-11-18 13:56:35 +0100

    yay puppies

--- pages/index.js
+++ pages/index.js
@@ -2,21 +2,45 @@ import { useEffect, useState } from 'react';
 
 export default function Home() {
     const [story, setStory] = useState(null);
+    const [assets, setAssets] = useState(null);
 
     useEffect(() => {
+        const token = 'YZlaiEASQmcqeE31jeOdNwtt-112339250310788-_R1ucy2NyfT_s2bPLDCc';
+        const headers = {
+            'Authorization': `${token}`,
+            'Content-Type': 'application/json'
+        }
+
         const fetchData = async () => {
             try {
                 const response = await fetch(
-                    `https://api.storyblok.com/v2/cdn/stories/interview_story?token=KsiGzs3TNPLsEPT4LyF7Awtt&version=published`
+                    `https://api.storyblok.com/v2/cdn/stories/interview_story?token=KsiGzs3TNPLsEPT4LyF7Awtt`
                 );
                 const data = await response.json();
                 setStory(data.story);
+                console.log(data.story);
+            } catch (error) {
+                console.log('Content not loaded yet');
+            }
+        };
+
+        const fetchAssets = async () => {
+            try {
+                const response = await fetch(
+                    `https://mapi.storyblok.com/v1/spaces/288497814063714/assets/`, {
+                    headers: headers
+                }
+                );
+                const data = await response.json();
+                setAssets(data.assets);
+                console.log(data.assets);
             } catch (error) {
                 console.log('Content not loaded yet');
             }
         };
 
         fetchData();
+        fetchAssets();
     }, []);
 
     if (!story) {
@@ -36,9 +60,14 @@ export default function Home() {
 
             <h2>{story.content?.title || 'Default Title'}</h2>
             <p>{story.content?.description || 'Default Description'}</p>
-            {story.content?.image && (
-                <img src={story.content.image.filename} alt="yay puppy" style={{ maxWidth: '300px' }} />
-            )}
+
+            <div>
+                {assets && assets.map((item, index) => (
+                    <img src={item.filename} alt="yay puppy" style={{ maxWidth: '300px' }} />
+                ))
+                }
+            </div>
+
         </div>
     );
 }
